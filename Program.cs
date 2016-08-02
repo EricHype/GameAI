4a67767031c0ae39cc73ade9d303fa28902d2dd5
Eric <eheitmuller@Erics-MacBook-Pro.local>
2016-08-01 21:27:31 -0700

    First commit, miner working

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FiniteState;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var miner = new Miner(0);
+
+            for(int i=0; i<20; i++){
+                miner.Update();
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
